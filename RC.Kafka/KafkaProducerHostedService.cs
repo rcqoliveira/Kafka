b33393d1e75286ea7ca4b54056a2cf2ba81afff8
rcqol <rcqol@DESKTOP-RMT1CKD>
2021-06-02 17:00:21 -0300

    #First commit

---
+++ RC.Kafka/KafkaProducerHostedService.cs
@@ -0,0 +1,47 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RC.Kafka
+{
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            var count = 0;
+            while (true)
+            {
+                var value = $"Producer - Hello Kafka - {count++}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("customer", new Message<Null, string> { Value = value }, cancellationToken);
+                _producer.Flush(timeout: TimeSpan.FromSeconds(10));
+
+                if (count == 100)
+                    break;
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ RC.Kafka/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace RC.Kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaConsumerHostedService>();
+                collection.AddHostedService<KafkaProducerHostedService>();
+            });
+    }
+}
